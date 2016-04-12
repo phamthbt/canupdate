0192421ade5b459881703f1d26ccc34fcb4559f4
Pham Phu Phi <phamphuphi@Pham-Phu-Phis-MacBook-Pro.local>
2016-04-12 10:39:47 +0700

    add module.php

--- module.php
+++ module.php
@@ -4,9 +4,17 @@
 <meta charset="UTF-8">
 <title>Untitled Document</title>
 </head>
+<div>Module 1</div>
 
+<div>
+	<ul>
+		<li>ok</li>
+		<li>ok</li>
+		<li>ok</li>
+		<li>ok</li>
+		<li>ok</li>
+	</ul>
+</div>
 <body>
-<div>version 1</div>
-
 </body>
 </html>
